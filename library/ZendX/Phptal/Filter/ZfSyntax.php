1d5b4e29b98091963aac3e06a19363484942702f
Tomek Pęszor <tomek@taat.pl>
2009-05-13 22:46:40 +0200

    Moved replaces() function to create_function()

--- library/ZendX/Phptal/Filter/ZfSyntax.php
+++ library/ZendX/Phptal/Filter/ZfSyntax.php
@@ -20,28 +20,19 @@ class ZendX_Phptal_Filter_ZfSyntax implements PHPTAL_Filter {
      */
     public function filter($xhtml)
     {
-        /**
-         * Callback helper function
-         * @param array $matches array of regex matches
-         * @return string
-         */
-        function replaces($matches) {
-            $search = array(
-                            // helpers
-                            '@\$this->([^\(\s;]+)\s?\((.*)\)@is',
-                            // variables
-                            '@\$this->([a-zA-z_0-9^\(]+)@is'
-                            );
-            $replace = array(
-                             '$ctx->this->\\1(\\2)',
-                             '$ctx->\\1'
-                             );
-            return preg_replace($search, $replace, $matches[1]);
-        }
-
         // finds PHP code block and performs _replace only inside
-        $xhtml = preg_replace_callback('@(<\?(=|php)?\s(.*?)\s\?>)@s', 'replaces', $xhtml);
-
+        $xhtml = preg_replace_callback('@(<\?(=|php)?\s(.*?)\s\?>)@s', create_function('$matches',
+       '$search = array(
+       // helpers
+       \'@\$this->([^\(\s;]+)\s?\((.*)\)@is\',
+                  // variables
+                  \'@\$this->([a-zA-z_0-9^\(]+)@is\'
+                                );
+                                $replace = array(
+                                \'$ctx->this->\\1(\\2)\',
+                                \'$ctx->\\1\'
+                                );
+                                return preg_replace($search, $replace, $matches[1]);'), $xhtml);
         return $xhtml;
     }
 }
